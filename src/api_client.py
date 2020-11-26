52241780f7bbfd27f2418a821f0dfc4ac84de294
Kevin Smith <kevin.smith@sprinthive.com>
2020-11-26 21:18:12 +0200

    Readme added, .gitignore added.

--- src/api_client.py
+++ src/api_client.py
@@ -6,6 +6,15 @@ class Client:
     def __init__(self, token):
         self.token = token
 
-    def get(self):
-        response = requests.get()
+        self.header = None
+        self.host = "openapi.investec.com"
+        self.domain = "za"
+        self.api = "pb"
+        self.version = "v1"
+        self.accounts = "accounts"
+        self.balance = "accounts{accountId}/balance"
+
+    def get(self, attribute):
+        print("Random crap")
+        # response = requests.get()
 
